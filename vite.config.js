a44cf443cc0caca2b28d2bd3d966ee04d6221ec3
Harshitha Rai <rainkharshitha.ai@gmail.com>
2025-12-08 02:42:53 -0500

    Use correct base for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Loanmaneger/',   // 👈 repo name + /  at both sides
+  base: '/',   // 👈 IMPORTANT for Vercel
 })
